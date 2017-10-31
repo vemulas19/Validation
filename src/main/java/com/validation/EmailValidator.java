fcf8df602fb9f91d523f4e13ea3623acd6360013
Vadlakonda-PC <Vadlakonda-PC@192.168.0.12>
2017-10-31 19:26:17 +0530

    This is My Second Validation in First Branch

--- src/main/java/com/validation/EmailValidator.java
+++ src/main/java/com/validation/EmailValidator.java
@@ -7,25 +7,8 @@ public class EmailValidator {
 
 		private Pattern pattern;
 		private Matcher matcher;
-
-		private static final String EMAIL_PATTERN =
-			"^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-			+ "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-
-		public EmailValidator() {
-			pattern = Pattern.compile(EMAIL_PATTERN);
-			System.out.println("helo");
-		}
-
-		/**
-		 * Validate hex with regular expression
-		 *
-		 * @param hex
-		 *            hex for validation
-		 * @return true valid hex, false invalid hex
-		 */
 		public boolean validate(final String hex) {
-
+			System.out.println("this is My Second Validation In First Branch");
 			matcher = pattern.matcher(hex);
 			System.out.println("hi");
 			return matcher.matches();

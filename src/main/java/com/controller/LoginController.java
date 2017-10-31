830326641f3566a4435dbf13473c0906efce9646
Vadlakonda-PC <Vadlakonda-PC@192.168.0.12>
2017-10-31 19:00:22 +0530

    Login,Security,validation Modified

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -1,12 +1,8 @@
 package com.controller;
 
 public class LoginController {
-	public static void main(String[] args) {
-		System.out.println("hi login"); 
-
-		System.out.println("Helo am from login controller");
-
-		System.out.println("welcome to my page");
+	public static void main(String[] args) { 
+		System.out.println("Hello This is Login Controller And This is your Venkatasai");
 	}
 
 }
--- src/main/java/com/controller/validation.java
+++ src/main/java/com/controller/validation.java
@@ -2,7 +2,7 @@ package com.controller;
 
 public class validation {
 	public void validateString(String[] args) {
-		System.out.println("validaiotn implemented");
+		System.out.println("This Is Venkatasai Validation Implimented");
 	}
 
 }

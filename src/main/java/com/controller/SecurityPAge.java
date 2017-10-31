9062eb7220593a116a985e53beede58880f17c3c
HOME <HOME@192.168.0.8>
2017-10-31 08:06:47 +0530

    secrutiy done.

---
+++ src/main/java/com/controller/SecurityPAge.java
@@ -0,0 +1,9 @@
+package com.controller;
+
+public class SecurityPAge {
+
+	public static void main(String[] args) {
+		System.out.println("Security implemented");
+		System.out.println("Interceptors are configured");
+	}
+}

11f7eb4e84b1cfae577ed4de27b5f3689dd29a11
SAGAR <SAGAR@192.168.0.4>
2017-10-31 08:13:38 +0530

    implemented review comments

--- src/main/java/com/controller/validation.java
+++ src/main/java/com/controller/validation.java
@@ -2,7 +2,7 @@ package com.controller;
 
 public class validation {
 	public static void main(String[] args) {
-		System.out.println("validation for string");
+		System.out.println("validaiotn implemented");
 	}
 
 }

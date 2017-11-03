c1b8f0566cc1d9376513f5b948290a85953a76a5
Vadlakonda-PC <Vadlakonda-PC@192.168.0.5>
2017-11-03 19:48:44 +0530

    Venkatasai

--- src/main/java/com/controller/LoginController.java
+++ src/main/java/com/controller/LoginController.java
@@ -2,7 +2,7 @@ package com.controller;
 
 public class LoginController {
 	public static void main(String[] args) { 
-		System.out.println("Hello This is Login Controller And This is your Venkatasai");
+		System.out.println("Hello this is Venkatasai");
 }
 
 }
